52da16be0418def991b43b322bf7c1b6551026ed
AndyRightNow <naruto950726@hotmail.com>
2016-04-28 10:23:01 +0800

    Add Tests

--- InputClass.js
+++ InputClass.js
@@ -12,3 +12,29 @@
 
 "use strict";
 
+var InputClass = {
+	//Mouse movement state
+    mouseMove: false,
+
+    //*******************************************
+    //	Mouse coordinates relative to the canvas    
+    //*******************************************
+    mouseX: 0,
+    mouseY: 0,
+
+    //**************************************
+    //	Initializa and shutdown
+    //**************************************
+    init : function(){},
+    shutdown : function(){},
+
+    //Listen to the user input and get the members data
+    listen : function(){},
+
+    //************************************************************
+    //	Compute the mouse coordinates relative to the canvas
+    //************************************************************
+    getMouseX : function(){},
+    getMouseY : function(){}
+};
+
