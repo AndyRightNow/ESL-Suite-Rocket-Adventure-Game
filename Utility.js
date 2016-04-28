9fa2ba38ea27fb382324d985e9589d7f09d77319
AndyRightNow <naruto950726@hotmail.com>
2016-04-28 23:46:17 +0800

    Stubbed out Utility and UtilityTest

--- Utility.js
+++ Utility.js
@@ -5,10 +5,38 @@
 
 	Overview:
 
-	Responsible for game record calculation and collision detection of objects.
-	It interacts with both UIClass and GraphicsClass.
+	Responsible for game record calculation, collision detection of objects, barrier 
+	generation and other computations. It interacts with both UIClass and GraphicsClass.
 
 *************************************************************************************/
 
 "use strict";
+
+//***********************
+//	General Utility
+//***********************
+var Utility = {
+	//*********************************************
+	//	Get a random index in within a range
+	//*********************************************
+	getRandIndex: function(upperbound){
+		return parseInt((Math.random() * upperbound + Math.random() * upperbound) % upperbound);
+	},
+
+	colliDetect: function(obj1, obj2){
+
+	}
+};
+
+//**********************
+//	Game Timer
+//**********************
+var Timer = {
+	lastTick: 0,
+	currTick: 0,
+	start: function(){},
+	stop: function(){},
+	reset: function(){},
+	tick: function(){}
+};
 
