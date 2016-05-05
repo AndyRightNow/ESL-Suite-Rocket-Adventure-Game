d559cf72f082d1af97b1e7ba162b4473865db1bb
AndyRightNow <naruto950726@hotmail.com>
2016-05-05 16:35:29 +0800

    Change to show the loading scene before click to start

--- EngineMain.js
+++ EngineMain.js
@@ -1,77 +1,77 @@
 /*************************************************************************************
-								ESL Suite Rocket Adventure Game
+                                ESL Suite Rocket Adventure Game
 
-								Created By Andy Zhou
+                                Created By Andy Zhou
 
 *************************************************************************************/
 
 "use strict";
 
-//	Game run count
+//  Game run count
 var gameRunCount = 0;
 
-//	Start to listen to the user input
+//  Start to listen to the user input
 InputClass.listen();
 
-var isStop = true, //	Game loop start and pause flag
+var isStop = true, //   Game loop start and pause flag
 
-    mouseX, mouseY; //	Mouse position
+    mouseX, mouseY; //  Mouse position
 
 //******************************
-//	Game background objects
+//  Game background objects
 //******************************
 var gameSkyWidth = 1257,
     gameSkyHeight = 400,
     backSkyPosX = gameSkyWidth - 27,
 
     gameSkys = [
-	new ImageObject(
+    new ImageObject(
             0,
             0,
             gameSkyWidth,
             gameSkyHeight),
 
-	new ImageObject(
-            backSkyPosX, //	Fix the border gap
+    new ImageObject(
+            backSkyPosX, // Fix the border gap
             0,
             gameSkyWidth,
             gameSkyHeight)],
 
-    frontSky = gameSkys[0], //	Front Sky background for alternating
-    backSky = gameSkys[1]; //	Back Sky background for alternating
+    frontSky = gameSkys[0], //  Front Sky background for alternating
+    backSky = gameSkys[1]; //   Back Sky background for alternating
 
 
 frontSky.addImageFrame("img/Background/Sky.jpg");
 backSky.addImageFrame("img/Background/Sky.jpg");
 
 //******************************
-//	Game sprite
+//  Game sprite
 //******************************
 var spriteWidth = 220,
     spriteHeight = 80,
-    spriteTopLeftPos = new Vector(GraphicsContext.width() * 0.05, 0), //	Position at the x% of the canvas width
+    spriteTopLeftPos = new Vector(GraphicsContext.width() * 0.05, 0), //    Position at the x% of the canvas width
     spriteCenterY,
     spriteCenterX,
-    spriteAngle = 0, //	Angle between the direction the sprite is point at and the x axis
-    spriteAy = 0, //	Vertical acceleration 
-    spriteAyCoef = 3.3, //	Acceleration coefficient, used to scale up or down the acceleration
+    spriteAngle = 0, // Angle between the direction the sprite is point at and the x axis
+    spriteAy = 0, //    Vertical acceleration 
+    spriteAyCoef = 3.3, //  Acceleration coefficient, used to scale up or down the acceleration
 
     spriteBoundingPoints = [
-					new Vector(91, 11),
-					new Vector(119, 8),
-					new Vector(133, 21),
-					new Vector(172, 21),
-					new Vector(204, 22),
-					new Vector(221, 27),
-					new Vector(236, 39),
-					new Vector(223, 51),
-					new Vector(207, 58),
-					new Vector(168, 59),
-					new Vector(134, 59),
-					new Vector(120, 71),
-					new Vector(91, 69),
-					new Vector(107, 57),
-					new Vector(105, 24)
+                    new Vector(91, 11),
+                    new Vector(119, 8),
+                    new Vector(133, 21),
+                    new Vector(172, 21),
+                    new Vector(204, 22),
+                    new Vector(221, 27),
+                    new Vector(236, 39),
+                    new Vector(223, 51),
+                    new Vector(207, 58),
+                    new Vector(168, 59),
+                    new Vector(134, 59),
+                    new Vector(120, 71),
+                    new Vector(91, 69),
+                    new Vector(107, 57),
+                    new Vector(105, 24)
     ],
     spriteBoundingBox = new Box(spriteTopLeftPos, spriteWidth, spriteHeight),
     spriteBounding = new Polygon(spriteBoundingPoints, spriteTopLeftPos, spriteBoundingBox),
@@ -83,7 +83,7 @@ var spriteWidth = 220,
         spriteHeight,
         spriteBounding),
 
-    spriteFrames = [ //	Use to compose sprite animation
+    spriteFrames = [ // Use to compose sprite animation
     "img/Sprite/1.png",
     "img/Sprite/2.png",
     "img/Sprite/3.png",
@@ -92,29 +92,29 @@ var spriteWidth = 220,
     "img/Sprite/2.png",
     "img/Sprite/1.png"];
 
-for (var i = 0; i < spriteFrames.length; i++) { //	Add frames to the sprite
+for (var i = 0; i < spriteFrames.length; i++) { //  Add frames to the sprite
     gameSprite.addImageFrame(spriteFrames[i]);
 }
 
 //*********************************************
-//	Global Game Objects parameters
+//  Global Game Objects parameters
 //*********************************************
 var g_GameObjectAx = -1,
     g_GameObjectAxDelta = 0.00001;
 
 //**************************
-//	Game Record and time
+//  Game Record and time
 //**************************
 var gameRecord = 0,
     gameRecordCoef = 0.8,
     highestGameRecord = 0,
     gameTime = 0;
 
-//	Barriers on the screen
+//  Barriers on the screen
 var barriersVisible = [];
 
 //**************************
-//	EngineMain Object
+//  EngineMain Object
 //**************************
 var EngineMain = {
     restart: function() {
@@ -124,9 +124,9 @@ var EngineMain = {
         g_GameObjectAxDelta = 0.00001;
 
         if (gameRecord > highestGameRecord) {
-            highestGameRecord = gameRecord;	//	Store the highest record
+            highestGameRecord = gameRecord; //  Store the highest record
         }
-        
+
         gameRecord = 0;
         gameRecordCoef = 0.5;
         gameTime = 0;
@@ -143,27 +143,27 @@ var EngineMain = {
 };
 
 //***********************************************************************
-//								Main game loop
-//	Use window.setInterval() in order not to hang the main UI thread 
+//                              Main game loop
+//  Use window.setInterval() in order not to hang the main UI thread 
 //***********************************************************************
 var MainGameLoop = setInterval(function() {
     //********************************
-    //	Check if the user click start
+    //  Check if the user click start
     //********************************
     if (InputClass.clickCount != 0) {
         isStop = false;
     }
-    //	Clear all
+    //  Clear all
     GraphicsContext.clearCanvas();
 
     //***************************
-    //	Get mouse position
+    //  Get mouse position
     //***************************
     mouseX = InputClass.mouseX;
     mouseY = InputClass.mouseY;
 
     //*******************************
-    //	Show basic game scene
+    //  Show basic game scene
     //*******************************
     spriteCenterY = gameSprite.getCenterY();
     spriteCenterX = gameSprite.getCenterX();
@@ -172,7 +172,7 @@ var MainGameLoop = setInterval(function() {
         spriteAy = (mouseY - spriteCenterY) * spriteAyCoef / GraphicsContext.height();
     }
 
-    //	Increase the control sensitivity with time
+    //  Increase the control sensitivity with time
     spriteAy += g_GameObjectAxDelta;
 
     spriteAngle = (Math.atan((mouseY - spriteCenterY) / (mouseX - spriteCenterX)) * (180 / Math.PI)) * 0.025;
@@ -186,66 +186,66 @@ var MainGameLoop = setInterval(function() {
     //****************************************************************DEBUG****************************************
     // GraphicsContext.drawPolygon(gameSprite.bounding, "blue");
 
-    //	The drawing below this line will be drawn under whatever is on the canvas
+    //  The drawing below this line will be drawn under whatever is on the canvas
     GraphicsContext.setGlobalComposition("destination-over");
 
     //**********************************************
-    //	Alternate between front and back sky images
+    //  Alternate between front and back sky images
     //**********************************************
     frontSky.update(0, g_GameObjectAx, 0);
     frontSky.draw();
     backSky.update(0, g_GameObjectAx, 0);
     backSky.draw();
-    if (backSky.x <= 0) { //	Swap front and back sky images
+    if (backSky.x <= 0) { //    Swap front and back sky images
         var tmp = frontSky;
         frontSky = backSky;
         backSky = tmp;
         backSky.x = backSkyPosX;
     }
 
-    //	The drawing below this line will be drawn on top of whatever is on the canvas
+    //  The drawing below this line will be drawn on top of whatever is on the canvas
     GraphicsContext.setGlobalComposition("source-over");
 
     //*************************************
-    //	Pause or show main game process
+    //  Pause or show main game process
     //*************************************
-    if (isStop) {
-        if (gameRunCount === 0) {
-            UIClass.showPauseScene(); //	Show pause scene
-        }
-        else{
-        	UIClass.showRestartScene(highestGameRecord);
-        }
+
+    if (!BarrierLoading.isDone() ||
+        !ImageLoading.isDone()) {
+        //  Show loading scene
+        UIClass.showLoadingScene();
     } else {
-        if (!BarrierLoading.isDone() ||
-            !ImageLoading.isDone()) {
-            //	Show loading scene
-            UIClass.showLoadingScene();
+        if (isStop) {
+            if (gameRunCount === 0) {
+                UIClass.showPauseScene(); //    Show pause scene
+            } else {
+                UIClass.showRestartScene(highestGameRecord);
+            }
         } else {
             //*************************************
-            //	Show main game process
+            //  Show main game process
             //*************************************
-            if (!Timer.isRunning()) { //	If the Timer is not started, start the counting
-                Timer.start(); //	Timer only start once
-                gameRunCount++; //	Game run count add one
+            if (!Timer.isRunning()) { //    If the Timer is not started, start the counting
+                Timer.start(); //   Timer only start once
+                gameRunCount++; //  Game run count add one
             }
 
             //********************************
-            //	Compute time and game record
+            //  Compute time and game record
             //********************************
             gameTime = Timer.totalTime();
             gameRecord = parseInt(gameTime * gameRecordCoef);
 
-            //	Increase the game objects speed
+            //  Increase the game objects speed
             g_GameObjectAx -= g_GameObjectAxDelta;
 
             //******************************************
-            //	Show barriers based on record
+            //  Show barriers based on record
             //******************************************
             BarrierGenerator.setLevel(gameRecord);
             var thisBarrier = BarrierGenerator.getThisBarrier(gameTime, g_GameObjectAx);
             if (thisBarrier !== null) {
-                barriersVisible.push(thisBarrier); //	If next barrier is available, push to the visible barriers array
+                barriersVisible.push(thisBarrier); //   If next barrier is available, push to the visible barriers array
             }
 
             for (var i = 0; i < barriersVisible.length; i++) {
@@ -253,15 +253,15 @@ var MainGameLoop = setInterval(function() {
                     barriersVisible[i].resetFlagAndPos(GraphicsContext.width(), 0);
                     barriersVisible.splice(i, 1);
                 }
-                barriersVisible[i].r += barriersVisible[i].dr;	//	Update rotation of the barrier
+                barriersVisible[i].r += barriersVisible[i].dr; //   Update rotation of the barrier
                 barriersVisible[i].update(
-                	barriersVisible[i].r, 
-                	barriersVisible[i].ax, 
-                	barriersVisible[i].ay);
+                    barriersVisible[i].r,
+                    barriersVisible[i].ax,
+                    barriersVisible[i].ay);
                 barriersVisible[i].draw();
-                if (barriersVisible[i].x <= gameSprite.x + gameSprite.width) {	//	If barrier top left x smaller than the sprite right x
+                if (barriersVisible[i].x <= gameSprite.x + gameSprite.width) { //   If barrier top left x smaller than the sprite right x
                     //**************************************
-                    //	If collided then restart the game
+                    //  If collided then restart the game
                     //**************************************
                     if (colliDetect.detect(gameSprite.bounding, barriersVisible[i].bounding)) {
                         EngineMain.restart();
@@ -271,10 +271,9 @@ var MainGameLoop = setInterval(function() {
                 // GraphicsContext.drawPolygon(barriersVisible[i].bounding, "blue");
             }
 
-
             //***********************************************************
-            //	Show game record at the top right corner of the canvas
-            //	and the highest record at the top left corner
+            //  Show game record at the top right corner of the canvas
+            //  and the highest record at the top left corner
             //***********************************************************
             UIClass.showGameRecord(gameRecord, highestGameRecord);
         }
