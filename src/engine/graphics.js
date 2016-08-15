a3041eb941a5bc1ddc348b33474e4b13f5680310
AndyRightNow <naruto950726@hotmail.com>
2016-08-15 18:59:38 +0800

    Refactor the graphics.js

--- src/engine/graphics.js
+++ src/engine/graphics.js
@@ -21,58 +21,67 @@ var GraphicsContext = {
     //  Get reference to the canvas context
     canvasCtx: null,
 
+    /*
+     * Init the graphics context
+     *
+     * @param {String} canvasId: The id of the canvas
+     */
     init: function(canvasId) {
         this._canvas = document.getElementById(canvasId);
 
         if (this._canvas) {
             this.canvasCtx = this._canvas.getContext("2d");
         }
+        else {
+            throw Error('Can not find the canvas with the id!');
+        }
     },
 
-    //******************************
-    //  Returne the size of canvas
-    //******************************
+    //---------------------------------
+    //  Canvas size and offset values
+    //---------------------------------
     width: function() {
         return this._canvas.clientWidth;
     },
     height: function() {
         return this._canvas.clientHeight;
     },
-
     offsetTop: function() {
         return this._canvas.offsetTop;
     },
-
     offsetLeft: function() {
         return this._canvas.offsetLeft;
     },
 
+    /*
+     * Return a reference to the canvas
+     */
     canvas: function() {
         return this._canvas;
     },
 
-    //***************************
-    //  Clear the whole canvas
-    //***************************
+    /*
+     *  Clear the whole canvas
+     */
     clearCanvas: function() {
         this.canvasCtx.clearRect(0, 0, this.width(), this.height());
     },
 
-    //************************************************
+    //-------------------------------------------------
+    //
     //  HTML5 Canvas intrinsic functions wrap-ups
-    //************************************************
+    //
+    //-------------------------------------------------
     drawImage: function(imgFrames, x, y, width, height) {
         return this.canvasCtx.drawImage(imgFrames, x, y, width, height);
 
     },
-
     drawCircle: function(circle, color) {
         this.canvasCtx.beginPath();
         this.canvasCtx.arc(circle.center.x, circle.center.y, circle.r, 0, Math.PI * 2);
         this.canvasCtx.fillStyle = color;
         this.canvasCtx.fill();
     },
-
     drawPolygon: function(poly, color) {
         this.canvasCtx.beginPath();
         for (var i = 0; i < poly.points.length; i++) {
@@ -89,31 +98,24 @@ var GraphicsContext = {
         this.canvasCtx.fill();
         this.canvasCtx.closePath();
     },
-
     translate: function(x, y) {
         return this.canvasCtx.translate(x, y);
     },
-
     rotate: function(angle) {
         return this.canvasCtx.rotate(angle);
     },
-
     scale: function(x, y) {
         return this.canvasCtx.scale(x, y);
     },
-
     transfrom: function(a, b, c, d, e, f) {
         return this.canvasCtx.transfrom(a, b, c, d, e, f);
     },
-
     save: function() {
         return this.canvasCtx.save();
     },
-
     restore: function() {
         return this.canvasCtx.restore();
     },
-
     setGlobalComposition: function(type) {
         this.canvasCtx.globalCompositeOperation = type;
     }
